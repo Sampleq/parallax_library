7c7b070a9439446bf8c6999ff8552917f607da86
Igor M. <samplefines@gmail.com>
2023-12-12 20:55:15 +0300

    impr adaptive, added transition = '0.033s linear

--- assets/src/scripts/my-rellax-analog.js
+++ assets/src/scripts/my-rellax-analog.js
@@ -1,14 +1,22 @@
 function translateOnScroll(array) {
     const biasedUnits = document.querySelectorAll(array);
 
+    for (let el of biasedUnits) {
+        el.style.transition = '0.033s linear';
+    }
+
     window.onscroll = () => {
         // console.warn('window.onscroll');
         // console.log(biasedUnits)
 
         for (let el of biasedUnits) {
             // console.log(el.getAttribute('data-rellax-speed'))
-
-            let speedCoeff = el.getAttribute('data-rellax-speed')
+            let speedCoeff
+            if (window.innerWidth <= 425 && el.getAttribute('data-rellax-mobile-speed')) {
+                speedCoeff = el.getAttribute('data-rellax-mobile-speed')
+            } else {
+                speedCoeff = el.getAttribute('data-rellax-speed')
+            }
             el.style.transform = `translate3d(0, ${(-Math.sin(speedCoeff / 10) * window.scrollY) * 0.6}px, 0)`;
         }
     }
